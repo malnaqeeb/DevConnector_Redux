e374c96889b20bbd3df5a3cbed533c93bb8fb4eb
unknown <mosleh.alnaqeeb@gmail.com>
2020-05-10 04:08:58 +0200

    setup redux with & Alert

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,93 @@
+import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
+
+const Register = ({setAlert}) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = formData;
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      setAlert("Password do not match", "danger"); // from alert.js : two arguments (msg, alertType) (danger here because that what we use in App.css)
+    } else {
+      console.log(formData);
+    }
+  };
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Create Your Account
+      </p>
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={(e) => onChange(e)}
+            minLength="6"
+           />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            value={password2}
+            onChange={(e) => onChange(e)}
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to="/ligin">Sign In</Link>
+      </p>
+    </Fragment>
+  );
+};
+Register.prototype ={
+    setAlert : PropTypes.func.isRequired,
+}
+// whenever you bring in an action you want to use it you have to pass it into connect
+// connect() use two things
+// First: state that you want to map (in our case we will pass null)
+// second: object with any actions you want to use (it allow us to access props.setAlert)
+export default connect(null, { setAlert })(Register);
